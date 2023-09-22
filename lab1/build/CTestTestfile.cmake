2e69e5d5cf2ce5b553195454e27ad0dae0aabb00
MeDGranD <ivanred289@gmail.com>
2023-09-22 09:36:17 +0300

    some tests changes

--- lab1/build/CTestTestfile.cmake
+++ lab1/build/CTestTestfile.cmake
@@ -4,5 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(lab1 "gtests")
+add_test(lab1 "lab1")
 set_tests_properties(lab1 PROPERTIES  _BACKTRACE_TRIPLES "/home/medgrand/Repositories/MAI_OOP/lab1/CMakeLists.txt;16;add_test;/home/medgrand/Repositories/MAI_OOP/lab1/CMakeLists.txt;0;")
